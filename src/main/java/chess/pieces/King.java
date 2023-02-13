4c19d0bcb84e0f9bc0bbd791b5556c3c0e0e1298
FernandoBrambilla <fernandobrambillab1@hotmail.com>
2023-02-13 15:09:18 -0300

    Add movimentos possíveis Rei

--- src/main/java/chess/pieces/King.java
+++ src/main/java/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardGame.Board;
+import boardGame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,9 +18,64 @@ import chess.Color;
          //Rei
      }
 
+    private boolean canMove(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p ==null || p.getColor() != getColor();
+     }
+     
     @Override
     public boolean[][] possibleMoves() {
         boolean [][] mat= new boolean [getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0, 0);
+        
+        //movimento acima
+        p.setValues(position.getRow()-1,position.getColumn());
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true; 
+        }
+        
+        //movimento abaixo
+        p.setValues(position.getRow()+1,position.getColumn());
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true; 
+        }
+        
+        //movimento esquerda
+        p.setValues(position.getRow(),position.getColumn()-1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true; 
+        }
+        
+        //movimento direita
+        p.setValues(position.getRow(),position.getColumn()+1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true; 
+        }
+        
+        //movimento diagonal noroeste
+        p.setValues(position.getRow()-1,position.getColumn()-1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true; 
+        }
+        
+        //movimento diagonal nordeste
+        p.setValues(position.getRow()-1,position.getColumn()+1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true; 
+        }
+        
+        //movimento diagonal sudoeste
+        p.setValues(position.getRow()+1,position.getColumn()-1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true; 
+        }
+        
+        //movimento diagonal sudeste
+        p.setValues(position.getRow()+1,position.getColumn()+1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true; 
+        }
+        
         return mat;
     }
     
