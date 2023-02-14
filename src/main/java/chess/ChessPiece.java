1aada409ae89593f0391a11add8040fb975561e9
FernandoBrambilla <fernandobrambillab1@hotmail.com>
2023-02-14 15:15:22 -0300

    Contador de Peças movidas

--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardGame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Color color, Board board) {
         super(board);
@@ -17,6 +18,17 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
     
+    public int getMoveCount() {
+        return moveCount;
+    }
+    
+    public void increaseMoveCount(){
+        moveCount ++;
+    }
+    
+    public void decreaseMoveCount(){
+        moveCount --;
+    }
     public ChessPosition getChesPosition(){
         return ChessPosition.fromPosition(position);
     }
